f54dfdd195842b239494e0e18161337f53079e48
sajjadahmad30 <sajjadahmad5578@gmail.com>
2024-09-29 10:53:29 +0500

    create onclick function on span

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ let inputDate=document.getElementsByTagName('input')
 let years=document.getElementsByClassName('years')
 let months=document.getElementsByClassName('months')
 let days=document.getElementsByClassName('days')
-let span =document.getElementsByTagName('span')
+let span =document.querySelector('span')
+
+span.onclick=function calcBirthDate(){
+  let date=inputDate.getFullYear();
+  console.log(date)
+}
