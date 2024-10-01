17511a8d38a3937529605587a0291f94296d629b
sajjadahmad30 <sajjadahmad5578@gmail.com>
2024-10-01 19:11:44 +0500

    create if statement for find the difference between birthDate and Today Date

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 let currentDate=document.getElementById('currentDate')
-let years=document.getElementsByClassName('years')
-let months=document.getElementsByClassName('months')
-let days=document.getElementsByClassName('days')
 let btn =document.getElementById('btn')
 
 btn.onclick=function calcBirthDate(){
@@ -15,4 +12,23 @@ btn.onclick=function calcBirthDate(){
   let y2= today.getFullYear();
    let m2= today.getMonth()+1;
    let d2= today.getDate();
+
+
+   // store the difference of years moths and date
+   let y3, m3, d3;
+   y3= y2-y1;
+
+   // create if stament to find the difference between birthDate and todayDate
+   if(m2 >= m1){
+      m3 = m2 - m1;
+   }else{
+      y3--;
+      m3 = 12 + m2 - m1;
+   }
+   // day difference
+   if(d2 >= d1){
+      d3 = d2 - d1;
+   }else{
+      m3--;
+   }
 }
